110fd77507022afdc25f0040f06044d4f9402041
syaina <syainanurf@gmail.com>
2021-05-05 06:47:35 +0700

    add User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
+
+    public function index()
+{
+        $getUser = User::OrderBy("id", "DESC")->paginate(50);
+
+        $out = [
+            'message' => 'get_user_success',
+            'results' => $getUser
+        ];
+
+        return response()->json($out, 200);
+    }
+}
